6ce0ff048d6cb15ac5b409bd59fd050fcb6e7cf5
Ashmita Haldar <67264881+ashmitahaldar@users.noreply.github.com>
2025-12-21 12:46:13 +0530

    chore: update Sanity and Vision dependencies to latest versions

--- frontend/src/services/sanityClient.js
+++ frontend/src/services/sanityClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID  || '2azshrlg', 
   dataset: 'production', 
   apiVersion: '2024-01-01', 
   useCdn: true, 
